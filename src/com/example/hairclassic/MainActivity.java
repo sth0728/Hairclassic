e777a14ac8d5d17163ebf3d738de195b4487b79c
sth0728 <taihwa.song@gmail.com>
2013-12-31 19:28:04 -0500

    more buttons in the main

--- src/com/example/hairclassic/MainActivity.java
+++ src/com/example/hairclassic/MainActivity.java
@@ -1,19 +1,24 @@
 package com.example.hairclassic;
 
+import java.util.Calendar;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
-	
+	private static TextView isOpenTxt;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		isOpenTxt = (TextView)findViewById(R.id.isopen);
 		
 		Button button = (Button)findViewById(R.id.hours);
 		button.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +29,8 @@ public class MainActivity extends Activity {
 				startActivity(intent);
 			}
 		});
+		
+		this.updateStatus();
 	}
 
 	@Override
@@ -32,5 +39,20 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	public void updateStatus(){
+		Calendar c = Calendar.getInstance();
+		int day = c.get(Calendar.DAY_OF_WEEK);
+		int hour = c.get(Calendar.HOUR_OF_DAY);
+		
+		if (day == 1){ // case Sunday
+			isOpenTxt.setText("Status: Closed");
+		} else if (hour >= 20 || hour < 11){
+			isOpenTxt.setText("Status: Closed");
+		} else {
+			isOpenTxt.setText("Status: Open (11am-8pm)");
+		}
+	}
 }
+
+
