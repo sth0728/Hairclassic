a912803bf028bd39762853036a0111a0e338d8f5
sth0728 <taihwa.song@gmail.com>
2014-01-06 23:12:30 -0500

    documentation done

--- src/com/example/hairclassic/style.java
+++ src/com/example/hairclassic/style.java
@@ -23,6 +23,7 @@ public class style extends Activity {
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		// on create
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.styles);
 	    
